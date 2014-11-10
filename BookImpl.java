14892ab20f92032a855e8c47426bcc84892905ab
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-10 14:38:46 +0000

    Created new class BookImpl

---
+++ BookImpl.java
@@ -0,0 +1,17 @@
+public class BookImpl implements Book{
+	private String author;
+	private String title;
+
+	public BookImpl(String author, String title) {
+		this.author = author;
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+}
