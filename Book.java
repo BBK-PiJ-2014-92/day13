a92b31922cad87b5345c950dfa6f86c6f8194262
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-10 14:38:25 +0000

    Created new implementation called book ex 1.1

---
+++ Book.java
@@ -0,0 +1,15 @@
+/** This is the interface for the class Book
+ * It contains a getter method for both the
+ * Author and Title of each book
+ */
+
+public interface Book {
+	/**
+	 * Returns the value of the field author
+	 */
+	public String getAuthor();
+	/**
+	 * Returns the value of the field title
+	 */
+	public String getTitle();
+}
